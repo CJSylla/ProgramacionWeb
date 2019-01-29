4a64ae2e3eb8323662fa6efd53f7af3ee2eada20
Claude Junior Sylla <cjsylla93@gmail.com>
2019-01-29 14:24:01 -0400

    Tarea 2 y 3 Done

--- src/main/java/com/Practica1/Main.java
+++ src/main/java/com/Practica1/Main.java
@@ -23,8 +23,14 @@ public class Main {
             // Para obtener el documento HTML de la URL
             Document documento = conexion.get();
 
-            System.out.println("\nTarea #1:");
+            System.out.println("\nTarea 1:");
             System.out.println("La cantidad de lineas del recurso es de: " + getCantLineasRecurso(respuesta));
+
+            System.out.println("\nTarea 2:");
+            System.out.println("La cantidad de <p> es de: " + getCantParrafos(documento));
+
+            System.out.println("\nTarea 3:");
+            System.out.println("La cantidad de <img> dentro de <p> es de: " + getCantImagParrafo(documento));
 
 
         } catch (IOException e) {
@@ -35,6 +41,14 @@ public class Main {
 
     private static int getCantLineasRecurso(Connection.Response recurso) {
         return recurso.body().split("\n").length;
+    }
+
+    private static int getCantParrafos(Document html) {
+        return html.select("p").size();
+    }
+
+    private static int getCantImagParrafo(Document html) {
+        return html.select("p img").size();
     }
 
 }
